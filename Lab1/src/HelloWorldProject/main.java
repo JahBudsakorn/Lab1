1dc980c0551fd45f25256c96ffac35cb7e024ccd
Asus <Asus@LAPTOP-ENF3J4UI>
2022-02-01 23:55:28 +0700

    Lab1

---
+++ Lab1/src/HelloWorldProject/main.java
@@ -0,0 +1,11 @@
+package HelloWorldProject;
+
+public class main {
+	
+	public main() {	
+	}//end function
+	public static void main(String[] args) {
+		System.out.println( "Hello World" ) ;
+	}//end function
+	
+}//end class
